1b89792a7b655492c900f80a9f3e63c977d1cd08
EmGaGa <EmGaGa@163.com>
2021-05-01 08:51:13 -0700

    transplant lvgl-7.11.0 demo

---
+++ code/header.h
@@ -0,0 +1,34 @@
+#ifndef __HEADER_H_
+#define __HEADER_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <linux/fb.h>
+#include "lvgl.h"
+#include "display/fbdev.h"
+#include "indev/evdev.h"
+
+#define	DISP_BUF_SIZE	(100 * LV_HOR_RES_MAX)
+
+/* debug */
+#if defined(PLAYER_DEBUG_VER)          /* DEBUG_VER */
+#define __PLAYER_DEBUG__                          1
+#endif
+
+/* log define */
+typedef enum
+{
+    DEBUG_TRACE_E = 0,
+    DEBUG_WARING_E, 
+    DEBUG_KEY_E,
+    DEBUG_ERROR_E,
+    /* MAX */
+    DEBUG_MAX_E = DEBUG_ERROR_E
+} debug_level_enum;
+
+
+
+#endif
